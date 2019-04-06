5ca336b1767d2b74a5395e90fca8cbc5b8711c7f
asl01 <alakoff@gmail.com>
2019-04-06 14:52:49 -0400

    Updates

---
+++ precache-manifest.908a31e7a53111a964f377612f929037.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "715effbbbe4788c7de9c",
+    "url": "/clicky-react/static/js/runtime~main.e9ac801e.js"
+  },
+  {
+    "revision": "9a6d50591ced2cbaff51",
+    "url": "/clicky-react/static/js/main.652edb56.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-react/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "9a6d50591ced2cbaff51",
+    "url": "/clicky-react/static/css/main.af79b0df.chunk.css"
+  },
+  {
+    "revision": "477f99612eb18651330de342e78ced0a",
+    "url": "/clicky-react/index.html"
+  }
+];
